c2e48b17b1e3c5c8b4c718109d7f5cbc34a7d3fc
mehedi hasan <mhshohan04@gmail.com>
2024-12-31 10:26:18 +0600

    use case of server and client compoents

---
+++ app/notes/server_vs_client.js
@@ -0,0 +1,10 @@
+// server side rendering vs server side Component
+// server side rendering--
+// 1.page level
+// 2.rendering on server
+// 3.full page rendering on server side
+// 4.components render on both server side and client side
+
+// server side component-
+// 1.component level
+// 2.rendering on server
